b455d7a2d4951fe45bb50d33fcff00c5d9eda4d5
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2021-11-10 18:26:11 +0200

    Add support for viewing GIFs using a URL

--- cmd/giffi.go
+++ cmd/giffi.go
@@ -3,6 +3,8 @@ package main
 import (
 	"image/gif"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/coocos/giffi/internal"
@@ -18,16 +20,29 @@ func readFileName() string {
 func loadGif(fileName string) (*gif.GIF, error) {
 	var image *gif.GIF
 
-	file, err := os.Open(fileName)
-	if err != nil {
-		return nil, err
+	// Load via HTTP if given file name seems to be a URL
+	if _, err := url.ParseRequestURI(fileName); err == nil {
+		resp, err := http.Get(fileName)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		image, err = gif.DecodeAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		file, err := os.Open(fileName)
+		if err != nil {
+			return nil, err
+		}
+		defer file.Close()
+		image, err = gif.DecodeAll(file)
+		if err != nil {
+			return nil, err
+		}
 	}
-	defer file.Close()
 
-	image, err = gif.DecodeAll(file)
-	if err != nil {
-		return nil, err
-	}
 	return image, nil
 }
 
